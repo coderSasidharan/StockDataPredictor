a967bbed5b5a65bc95df2b5b03883fdc6ea552ba
coderSasidharan <66076670+coderSasidharan@users.noreply.github.com>
2025-01-04 13:20:44 -0500

    Update stockPredictor.py

--- stockPredictor.py
+++ stockPredictor.py
@@ -9,7 +9,9 @@ import plotly.graph_objects as go
 START = "2010-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title = ("Stock Prediction App")
+st.title = ("Stock Track")
+
+st.markdown("# Stock Track")
 
 stock = st.text_input("Enter the name of the stock: ")
 n_years = st.slider("Years for prediction: ", 1 , 10)
@@ -26,19 +28,17 @@ def load_data(value):
     data.reset_index(inplace=True)
     return data
 
-data_load_state = st.text("Download Data...")
+data_load_state = st.text("Downloading Data...")
 
 data = load_data(stock)
 data_load_state.text("Data Uploaded")
 
-st.subheader('Raw Data')
-st.write(data.tail())
+
 
 def plot_raw_data():
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y= data['Open'], name = 'stock_open'))
     fig.add_trace(go.Scatter(x=data['Date'], y= data['Close'], name = 'stock_close'))
-    fig.layout.update(title_text="Past to Current Data", xaxis_rangeslider_visible=True)
+    fig.layout.update(title_text="Past Prices", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 plot_raw_data()
@@ -55,17 +55,36 @@ future = model.make_future_dataframe(periods = period)
 forecast = model.predict(future)
 
 st.subheader('Future prediction')
-st.write(forecast.tail())
 
 fig1 = plot_plotly(model, forecast)
 st.plotly_chart(fig1)
 
-st.write('forecast components')
-fig2 = model.plot_components(forecast)
-st.write(fig2)
 
 
+st.subheader('Compare Stocks Past Trends')
 
 
+stock1 = st.text_input("Enter the name of the first stock: ")
+stock2 = st.text_input("Enter the name of the second stock: ")
 
 
+if (not stock1 or not stock2):
+    st.info('The program will run once you have entered stocks.')
+    st.stop()
+
+
+data_load_state = st.text("Downloading Data...")
+
+data1 = load_data(stock1)
+data2 = load_data(stock2)
+data_load_state.text("Data Uploaded")
+
+
+fig = go.Figure()
+def plot_compare_data():
+    fig.add_trace(go.Scatter(x=data1['Date'], y= data1['Close'], name = stock1))
+    fig.add_trace(go.Scatter(x=data2['Date'], y= data2['Close'], name = stock2))
+    fig.layout.update(title_text="Past Prices", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_compare_data()
